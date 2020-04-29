00fdd1d45437f1ee146318f38b7078a2575d8cdd
Luka Jovanovic <lujovanovic7@gmail.com>
2020-04-29 22:55:37 +0200

    urednije

--- generateRandomExample.m
+++ generateRandomExample.m
@@ -1,16 +1,16 @@
 function generateRandomExample(n, m, del, degree, test_filename)
-X = [];
-for i = 1:n
-	X = [X linspace(-10, 10, m)'];
-endfor
+	X = [];
+	for i = 1:n
+		X = [X linspace(-10, 10, m)'];
+	endfor
 
-theta = rand(n, 1) * 10;
-y = X * theta + (rand(m, 1) * (2 * del) .- del);
+	theta = rand(n, 1) * 10;
+	y = X * theta + (rand(m, 1) * (2 * del) .- del);
 
-for i=2:degree
-	X(:, end+1) = X(:, 1) .^ i;
-endfor
+	for i=2:degree
+		X(:, end+1) = X(:, 1) .^ i;
+	endfor
 
-save(test_filename, "X", "y");
+	save(test_filename, "X", "y");
 
 end
--- mapFeature.m
+++
@@ -1,2 +0,0 @@
-function out = mapFeature(X)
-end
