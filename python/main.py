fdfa65b8b72e44e88af9a776c4228fadd864b8f9
Luka Jovanovic <lujovanovic7@gmail.com>
2020-05-17 00:11:16 +0200

    deleted estimate function

--- python/main.py
+++ python/main.py
@@ -107,7 +107,9 @@ def load_data(new_data, machine_name, sensor_list, start, end):
 	for cur_sensor in sensor_list:
 		new_data[ cur_sensor ] = []
 		filter.filtered_data(new_data[ cur_sensor ], machine_name, cur_sensor, start, None, end)
-	
+
+
+def 
 
 #######NEPOTREBNO KASNIJE
 CUR_MACHINE = "FL01"
@@ -164,54 +166,6 @@ def plotMeasurementsDistribution(start = 0, duration = None, end = inf):
 	filter.filtered_data(meas_list, CUR_MACHINE, CUR_SENSOR, start, duration, end)
 	meas_v = filter.measurements_to_numpy_vector(meas_list)
 	vis.Plot(meas_v, kind = 'hist', bins = 100)
-
-
-def estimate(start_train = 0, duration_train = None, end_train = inf, start_outlier = 0, duration_outlier = None, end_outlier = inf):
-	global CUR_MACHINE, CUR_SENSOR, mu, Sigma2, epsilon, F1, estimated
-	print("Estimating for:\n..Machine:", CUR_MACHINE, "\n..Sensor:", CUR_SENSOR)
-	if CUR_MACHINE == "":
-		print("Please select machine")
-		return
-	train_list = []
-	outlier_list = []
-	filter.filtered_data(train_list, CUR_MACHINE, CUR_SENSOR, start_train, duration_train, end_train)
-	filter.filtered_data(outlier_list, CUR_MACHINE, CUR_SENSOR, start_outlier, duration_outlier, end_outlier)
-	
-	train_v = filter.measurements_to_numpy_vector(train_list)[:, None] #Making it transposed
-	M_total = train_v.shape[ 0 ]
-	M_train = int(M_total * 0.7)
-	M_cvs_good = M_total - M_train
-	M_cvs_outlier = len(outlier_list)
-	
-	perm = np.random.permutation(M_total)
-	cvs_good_v = train_v[ perm[0:M_cvs_good] ]
-	train_v = train_v[ perm[M_cvs_good:M_total] ]
-	cvs_outlier_v = filter.measurements_to_numpy_vector(outlier_list)[:, None] #Making it transposed
-	
-	print("M_train", M_train, train_v.shape[ 0 ])	
-	print("M_cvs_good", M_cvs_good, cvs_good_v.shape[ 0 ])
-	print("M_cvs_outlier", M_cvs_outlier, cvs_outlier_v.shape[ 0 ])
-	
-	print("Plotting histogram of Train data, should resemble Gaussian")
-	vis.Plot(train_v[:, 0], kind = 'density')
-	vis.Plot(train_v[:, 0], kind = "hist", bins = 50)
-	
-	mu, Sigma2 = ad.estimateMultivariateGaussian(train_v)
-	estimated = True
-	print("Estimation finished.", "Mu found:", mu, "Sigma2 found:", Sigma2, sep = "\n")
-	
-	y_cvs_all = np.hstack((np.zeros(M_cvs_good, dtype=bool), np.ones(M_cvs_outlier, dtype=bool)))
-	p_cvs_all = ad.multivariateGaussian(np.vstack((cvs_good_v, cvs_outlier_v)), mu, Sigma2)
-	
-	print("Selecting epsilon...")
-	epsilon, F1 = ad.selectThreshold(y_cvs_all, p_cvs_all)
-	print("Best epsilon:", epsilon, "\nGives F1:", F1)
-	
-	#circle all found outliers
-	cvs_outlier_cnt = sum(p_cvs_all[M_cvs_good:] < epsilon) #found outliers out of all outliers
-	cvs_good_cnt = sum(p_cvs_all[0:M_cvs_good] >= epsilon) #found good measures out of all good measures
-	print("Out of all outliers, we predicted ", cvs_outlier_cnt, "/", M_cvs_outlier, " to be outlier", sep="")
-	print("Out of all good measurements, we predicted ", cvs_good_cnt, "/", M_cvs_good, " to be good", sep = "")	
 
 
 ###EXAMPLE FUNCTIONS
